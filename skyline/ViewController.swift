6e6fc5952d0247d3304909302a787448c112aee4
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-02-25 17:03:54 +0900

    initial commit

--- skyline/ViewController.swift
+++ skyline/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var textView: UITextView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		setNeedsStatusBarAppearanceUpdate();
+	}
+
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		UIApplication.shared.statusBarStyle = .default
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	override var prefersStatusBarHidden: Bool { // trueの場合はステータスバー非表示
+		return false
+	}
+
+	override var preferredStatusBarStyle: UIStatusBarStyle { // ステータスバーを白くする
+		return .lightContent;
+	}
 
 }
 
