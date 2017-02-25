0cf695721ed8864495023cc18280572cb0bc04a1
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-02-25 17:51:28 +0900

    textattributes manager

---
+++ skyline/TextAttributesManager.swift
@@ -0,0 +1,23 @@
+//
+//  TextAttributesManager.swift
+//  skyline
+//
+//  Created by Keisei Saito on 2017/2/25.
+//  Copyright © 2017年 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class TextAttributesManager {
+
+	static let shared = TextAttributesManager()
+
+	var hoge = "" // example :]
+
+	private init() {
+
+	}
+
+	// do something!
+
+}
